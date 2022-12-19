793c9b4b808e606c93a0447c88f4b9e857fd1fc0
EmrullahKutlar <emrullah.ktlr@gmail.com>
2022-12-19 10:39:08 +0300

    gereksiz yerler silindi

--- my-project/src/components/BlogList.js
+++ my-project/src/components/BlogList.js
@@ -4,6 +4,12 @@ const BlogList = (props) => {
   // yada ({blogs, title}) şeklinde de yazılabilir
   const blogs = props.blogs;
 
+  // const deneme= (<p> Written By-  {blogs[1].author} </p>)
+  
+const getId=(id,e)=>{
+  console.log("this is: ", id);
+  console.log("e is: ", e);
+}
   return (
     <div className="blog-List">
       <h2> {props.title} </h2>
@@ -13,7 +19,9 @@ const BlogList = (props) => {
           <Link to={`/blogs/${blog.id}`}>
             <h2> {blog.title} </h2>
             <p> Written by - {blog.author} </p>
+            {/* {deneme} */}
           </Link>
+          <button id="butonId" onClick={(e)=> getId(blog.id , e.target.id)}> Click</button>
         </div>
       ))}
     </div>
