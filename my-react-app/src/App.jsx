36caf4d2cf987f702e5dfd6ce3b0d40bc80b8c0b
EmrullahKutlar <emrullah.ktlr@gmail.com>
2022-12-20 14:06:33 +0300

    ReactJS Course [4] - UseState Hook | States in React Tutorial

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,77 +1,48 @@
 //sfc
 import { useState } from "react";
-import User from "./User";
 
 function App() {
-  const age = 15;
-  const isGreen = true;
+  const [age, setAge] = useState(0);
 
-  // if(age>18){
-  //   return <h1>Over Age</h1>
-  // }else{
-  //   return <h1>Under Age</h1>
-  // }
-  const names = ["John", "Bob", "Mary"];
-  const users=[
-    {name:"John",age:20},
-    {name:"Bob",age:25},
-    {name:"Mary",age:30},
-    {name:"Emrullah",age:23}
-  ]
+  const increaseAge = () => {
+    setAge(age + 1);
+  };
 
-  const planets=[
-    {name:"Mars", isGasPlanet:false},
-    {name:"Jupiter", isGasPlanet:true},
-    {name:"Saturn", isGasPlanet:true},
-    {name:"Earth", isGasPlanet:false},
-    {name:"Venus", isGasPlanet:false},
-    {name:"Mercury", isGasPlanet:false},
-    {name:"Uranus", isGasPlanet:true},
-    {name:"Neptune", isGasPlanet:true},
-    {name:"Pluto", isGasPlanet:false}
-  ]
+  const [inputValue, setInputValue] = useState("");
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const [show,setShow]=useState(false)
+
+  const [number, setNumber]=useState(0)
 
   return (
     <div className="App">
-      {age >= 18 ? <h1>Over Age</h1> : <h1>Under Age</h1>}
-      <h1 style={{ color: isGreen ? "green" : "red" }}> THIS HAS COLOR </h1>
-
-      {/* map ile yazdırma işlemi */}
-
-      {names.map((name, index) => {
-        return <h1 key={index}> {index+1} -{name}</h1>;
-      })}
+      <h1>Age: {age}</h1>
+      <button onClick={increaseAge}>Increase Age</button>
+      <input
+        type="text"
+        value={inputValue}
+        onChange={(e) => {
+          setInputValue(e.target.value);
+        }}
+      />
+      <input type="text" value={inputValue} onChange={handleInputChange} />
+      <p>{inputValue}</p>
       <hr />
-      {users.map((user, index) => {
-        return <h1 key={index}> {index+1} -{user.name} , {user.age}</h1>;
-      })}
+      <button onClick={()=> setShow(!show)}>press to toggle it</button>
+      {show && <h1 >Now You Can See </h1>}
+      <h1 style={{color:show ?'green':'red'}}> {show.toString()} </h1>
       <hr />
-      {users.map((user, index) => {
-        return <User key={index} number={index+1} name={user.name} age={user.age}  />;
-      })}
-
-      {/* Exercise */}
-      <hr />
-      {/* planets dizisinin her objesini dolaşıp isGasPlanet == true olanları map ile yazdırma */}
-      {planets.filter(planet=>planet.isGasPlanet).map((planet,index)=>{
-        return <h1 key={index}>{index+1} - {planet.name} is a gas planet</h1>
-        })
-      }
-      {/* yada */}
-      <hr />
-      {planets.map((planet,index)=>{
-       return planet.isGasPlanet && <h1 key={index}>{index} - {planet.name} is a gas planet</h1>
-       } )
-      }
+      <p> {number} </p> <br />
+      <button onClick={()=> setNumber(number+1)}>Arttır </button>
+      <button onClick={()=> setNumber(number-1)} >Azalt </button>
+      <button onClick={()=> setNumber(0)}> Sıfırla </button>
+      
     </div>
   );
 }
 
-
-// const User = (props) => {
-//   return ( 
-//     <h1>{props.index} - {props.name},{props.age}</h1>
-//    );
-// }
- 
 export default App;
--- my-react-app/src/User.jsx
+++
@@ -1,9 +0,0 @@
-const User = (props) => {
-  return (
-    <h1>
-      {props.number} - {props.name}, {props.age}
-    </h1>
-  );
-};
-
-export default User;
