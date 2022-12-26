c353dbcda9a6e50fd2145c3a2a5c854889dad01f
EmrullahKutlar <emrullah.ktlr@gmail.com>
2022-12-26 11:53:46 +0300

    some css

--- my-react-app/src/Navbar.jsx
+++ my-react-app/src/Navbar.jsx
@@ -1,11 +1,18 @@
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
-    return ( <div>Navbar 
-        <Link to={'/'}>Home</Link>
-        <Link to={'/menu'}>Menu</Link>
-        <Link to={'/contact'}>Contact</Link>
-      </div> );
-}
- 
+  return (
+    <div className="container ">
+      <div className="row">
+        <div className="col d-flex justify-content-around mt-5">
+          Navbar
+          <Link to={"/"}>Home</Link>
+          <Link to={"/menu"}>Menu</Link>
+          <Link to={"/contact"}>Contact</Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 export default Navbar;
