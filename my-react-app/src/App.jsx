7d66acab8bf06c662183951726fef360332bfbd8
EmrullahKutlar <emrullah.ktlr@gmail.com>
2022-12-26 12:18:12 +0300

    10-Fetch-Data-With-React-Query

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -4,25 +4,36 @@ import Home from "./pages/Home";
 import Menu from "./pages/Menu";
 import Contact from "./pages/Contact";
 import Navbar from "./Navbar";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 function App() {
+  const client = new QueryClient({defaultOptions:{
+      queries:{
+        refetchOnWindowFocus:false 
+        // sayfa degistirince refetch yapmasını onluyor.
+        //Tutoriala bak 
+      }
+  }});
+
   return (
     <div className="App">
-      <Router>
-        <Navbar />
-        <div className="container">
-          <div className="row">
-            <div className="col-12 mt-5 d-flex justify-content-center">
-              <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/menu" element={<Menu />} />
-                <Route path="/contact" element={<Contact />} />
-                <Route path="*" element={<h1>Page Not Found</h1>} />
-              </Routes>
+      <QueryClientProvider client={client}>
+        <Router>
+          <Navbar />
+          <div className="container">
+            <div className="row">
+              <div className="col-12 mt-5 d-flex justify-content-center">
+                <Routes>
+                  <Route path="/" element={<Home />} />
+                  <Route path="/menu" element={<Menu />} />
+                  <Route path="/contact" element={<Contact />} />
+                  <Route path="*" element={<h1>Page Not Found</h1>} />
+                </Routes>
+              </div>
             </div>
           </div>
-        </div>
-      </Router>
+        </Router>
+      </QueryClientProvider>
     </div>
   );
 }
--- my-react-app/src/pages/Home.jsx
+++ my-react-app/src/pages/Home.jsx
@@ -1,9 +1,24 @@
+import { useEffect } from "react";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+function Home() {
+  const {data:CatData, isLoading, isError, refetch} = useQuery(["cat"], () => {
+   return axios.get("https://catfact.ninja/fact").then((res) => res.data);
+    
+  });
+  if(isError){
+    return <h1>Sorry there was an error</h1>
+  }
 
-function Home  () {
-    return ( 
-        <div>
-            <h1>This is Home Page</h1>
-        </div>
-     );
+  if(isLoading){
+    return <h1>Loading</h1>
+  }
+  return (
+    <div>
+      <h1>This is Home Page</h1>
+      <h4> {CatData?.fact} </h4>
+      <button className="btn btn-info" onClick={refetch}> refetch</button>
+    </div>
+  );
 }
 export default Home;
